edb65dbd6fc561cd83f576a787caa16e417d5945
aldoprogrammer <aldobesma@gmail.com>
2024-02-05 18:49:55 +0700

    update

---
+++ resources/views/admin/admin_profile_view.blade.php
@@ -0,0 +1,18 @@
+@extends('admin.admin_master')
+@section('content')
+
+
+
+<div class="container-full">
+
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="row">
+
+        </div>
+    </section>
+    <!-- /.content -->
+  </div>
+
+@endsection
