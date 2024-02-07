34a1265ff0aaf80d0f1c5229f7a410a762efa155
aldoprogrammer <aldobesma@gmail.com>
2024-02-07 12:10:30 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -59,28 +59,76 @@
 <div class="col-md-6 col-sm-6 create-new-account">
 	<h4 class="checkout-subtitle">Create a new account</h4>
 	<p class="text title-tag-line">Create your new account.</p>
-	<form class="register-form outer-top-xs" role="form">
+	<form class="register-form outer-top-xs" role="form"  action="{{ route('register') }}"
+    method="POST">
+    @csrf
 		<div class="form-group">
-	    	<label class="info-title" for="exampleInputEmail2">Email Address <span>*</span></label>
-	    	<input type="email" class="form-control unicase-form-control text-input" id="exampleInputEmail2" >
-	  	</div>
-        <div class="form-group">
 		    <label class="info-title" for="name">Name <span>*</span></label>
-		    <input type="email" class="form-control unicase-form-control text-input" id="exampleInputEmail1" >
+		    <input type="text"
+			class="form-control
+			unicase-form-control text-input"
+			id="name" name="name" >
+            @error('name')
+                <span class="invalid-feedback"
+                role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
 		</div>
+		<div class="form-group">
+	    	<label class="info-title"
+			for="exampleInputEmail2">Email Address <span>*</span></label>
+	    	<input type="email" class="form-control unicase-form-control
+			text-input" id="email" name="email" >
+            @error('email')
+                <span class="invalid-feedback"
+                role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+	  	</div>
+
         <div class="form-group">
 		    <label class="info-title" for="phone">Phone Number <span>*</span></label>
-		    <input type="email" class="form-control unicase-form-control text-input" id="exampleInputEmail1" >
+		    <input type="text"
+			class="form-control
+			unicase-form-control
+			text-input" id="phone" name="phone" >
+            @error('phone')
+                <span class="invalid-feedback"
+                role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
 		</div>
         <div class="form-group">
 		    <label class="info-title" for="password">Password <span>*</span></label>
-		    <input type="email" class="form-control unicase-form-control text-input" id="exampleInputEmail1" >
+		    <input type="password"
+			class="form-control
+			unicase-form-control text-input"
+			id="password" name="password" >
+            @error('password')
+                <span class="invalid-feedback"
+                role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
 		</div>
          <div class="form-group">
 		    <label class="info-title" for="password_confirmation">Confirm Password <span>*</span></label>
-		    <input type="email" class="form-control unicase-form-control text-input" id="exampleInputEmail1" >
+		    <input type="password" class="form-control
+			unicase-form-control text-input"
+			id="password_confirmation" >
+            @error('password_confirmation')
+                <span class="invalid-feedback"
+                role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
 		</div>
-	  	<button type="submit" class="btn-upper btn btn-primary checkout-page-button">Sign Up</button>
+	  	<button type="submit" class="btn-upper
+		btn btn-primary checkout-page-button">
+		Sign Up</button>
 	</form>
 
 
