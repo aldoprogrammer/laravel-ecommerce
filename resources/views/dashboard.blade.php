73d7e5760ba4b9f24803dab30c79f25f58855353
aldoprogrammer <aldobesma@gmail.com>
2024-02-08 08:53:22 +0700

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,49 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-           Hi..{{ Auth::user()->name }}
-        </h2>
-    </x-slot>
+@extends('frontend.main_master')
+@section('content')
 
-    <div class="py-12">
-        This is just home page 
+    <div class="body-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2">
+                    <br>
+                    <img class="card-img-top mt-3 mb-3"
+                    src="{{ (!empty($adminData->profile_photo_path))
+                      ? url('upload/admin_images/'.$adminData->profile_photo_path)
+                      : url('upload/no_image.jpg')}}"
+                    alt="User Avatar"
+                    style="border-radius: 50%;" height="100%" width="100%"
+                    >
+                    <li class="list-group list-group-flush">
+                        <a href="" class="btn btn-primary btn-sm
+                        btn-block">
+                            Home
+                        </a>
+                        <a href="" class="btn btn-primary btn-sm
+                        btn-block">
+                            Profile Update
+                        </a>
+                        <a href="" class="btn btn-primary btn-sm
+                        btn-block">
+                            Change Password
+                        </a>
+                        <a href="" class="btn btn-danger btn-sm
+                        btn-block">
+                            Logout
+                        </a>
+                    </li>
+                </div>
+                <div class="col-md-2"></div>
+                <div class="col-md-6">
+                    <div class="card">
+                        <h3 class="text-center">
+                            <span class="text-danger">Hi....</span>
+                            <strong>{{ Auth::user()->name }}</strong>
+                            Welcome to Dashboard
+                        </h3>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</x-app-layout>
+
+@endsection
