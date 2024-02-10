57f888b387fa554e68182f3d3f578b463d108ed4
aldoprogrammer <aldobesma@gmail.com>
2024-02-10 11:36:59 +0700

    update

--- resources/views/admin/brands/brand_view.blade.php
+++ resources/views/admin/brands/brand_view.blade.php
@@ -1,53 +1,143 @@
 @extends('admin.admin_master')
 @section('content')
+    <section class="content">
+        <div class="row">
+            <div class="col-8">
+                <div class="box">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Data Table Brands</h3>
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="table-responsive">
+                            <div id="example1_wrapper" class="dataTables_wrapper container-fluid dt-bootstrap4">
+                                <div class="row">
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <table id="example1" class="table table-bordered table-striped dataTable"
+                                            role="grid" aria-describedby="example1_info">
+                                            <thead>
+                                                <tr role="row">
+                                                    <th>Brand En</th>
+                                                    <th>Brand Ind</th>
+                                                    <th>Brand Image</th>
+                                                    <th>Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                @foreach ($brands as $item)
+                                                    <tr role="row" class="odd">
+                                                        <td>{{ $item->brand_name_en }}</td>
+                                                        <td>{{ $item->brand_name_ind }}</td>
+                                                        <td><img src="{{ $item->brand_image }}" alt=""></td>
+                                                        <td>
+                                                            <a href="{{ route('brand.edit', $item->id) }}"
+                                                                class="btn btn-info">Edit</a>
+                                                            <a href="{{ route('brand.delete', $item->id) }}"
+                                                                class="btn btn-danger">Delete</a>
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
 
-<section class="content">
-    <div class="row">
-<div class="col-12">
-    <div class="box">
-       <div class="box-header with-border">
-         <h3 class="box-title">Data Table Brands</h3>
-       </div>
-       <!-- /.box-header -->
-       <div class="box-body">
-           <div class="table-responsive">
-             <div id="example1_wrapper"
-             class="dataTables_wrapper container-fluid dt-bootstrap4"><div class="row">
-</div><div class="row"><div class="col-sm-12"><table id="example1" class="table table-bordered table-striped dataTable" role="grid" aria-describedby="example1_info">
-               <thead>
-                   <tr role="row">
-                    <th>Brand En</th>
-                    <th>Brand Ind</th>
-                    <th>Brand Image</th>
-                    <th>Action</th>
-                    </tr>
-               </thead>
-               <tbody>
-                @foreach ($brands as $item)
-                <tr role="row" class="odd">
-                    <td>{{$item->brand_name_en}}</td>
-                    <td>{{$item->brand_name_ind}}</td>
-                    <td><img src="{{$item->brand_image}}"
-                        alt=""></td>
-                    <td>
-                        <a href="{{route('brand.edit',$item->id)}}"
-                            class="btn btn-info">Edit</a>
-                        <a href="{{route('brand.delete',$item->id)}}"
-                            class="btn btn-danger">Delete</a>
-                    </td>
-                </tr>
-                @endforeach
 
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-12 col-md-5">
+                                        <div class="dataTables_info" id="example1_info" role="status" aria-live="polite">
+                                            Showing 1 to 10 of 57 entries</div>
+                                    </div>
+                                    <div class="col-sm-12 col-md-7">
+                                        <div class="dataTables_paginate paging_simple_numbers" id="example1_paginate">
+                                            <ul class="pagination">
+                                                <li class="paginate_button page-item previous disabled"
+                                                    id="example1_previous"><a href="#" aria-controls="example1"
+                                                        data-dt-idx="0" tabindex="0" class="page-link">Previous</a></li>
+                                                <li class="paginate_button page-item active"><a href="#"
+                                                        aria-controls="example1" data-dt-idx="1" tabindex="0"
+                                                        class="page-link">1</a></li>
+                                                <li class="paginate_button page-item "><a href="#"
+                                                        aria-controls="example1" data-dt-idx="2" tabindex="0"
+                                                        class="page-link">2</a></li>
+                                                <li class="paginate_button page-item "><a href="#"
+                                                        aria-controls="example1" data-dt-idx="3" tabindex="0"
+                                                        class="page-link">3</a></li>
+                                                <li class="paginate_button page-item "><a href="#"
+                                                        aria-controls="example1" data-dt-idx="4" tabindex="0"
+                                                        class="page-link">4</a></li>
+                                                <li class="paginate_button page-item "><a href="#"
+                                                        aria-controls="example1" data-dt-idx="5" tabindex="0"
+                                                        class="page-link">5</a></li>
+                                                <li class="paginate_button page-item "><a href="#"
+                                                        aria-controls="example1" data-dt-idx="6" tabindex="0"
+                                                        class="page-link">6</a></li>
+                                                <li class="paginate_button page-item next" id="example1_next"><a
+                                                        href="#" aria-controls="example1" data-dt-idx="7"
+                                                        tabindex="0" class="page-link">Next</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
 
-                </tbody>
-             </table></div></div><div class="row"><div class="col-sm-12 col-md-5"><div class="dataTables_info" id="example1_info" role="status" aria-live="polite">Showing 1 to 10 of 57 entries</div></div><div class="col-sm-12 col-md-7"><div class="dataTables_paginate paging_simple_numbers" id="example1_paginate"><ul class="pagination"><li class="paginate_button page-item previous disabled" id="example1_previous"><a href="#" aria-controls="example1" data-dt-idx="0" tabindex="0" class="page-link">Previous</a></li><li class="paginate_button page-item active"><a href="#" aria-controls="example1" data-dt-idx="1" tabindex="0" class="page-link">1</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="2" tabindex="0" class="page-link">2</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="3" tabindex="0" class="page-link">3</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="4" tabindex="0" class="page-link">4</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="5" tabindex="0" class="page-link">5</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="6" tabindex="0" class="page-link">6</a></li><li class="paginate_button page-item next" id="example1_next"><a href="#" aria-controls="example1" data-dt-idx="7" tabindex="0" class="page-link">Next</a></li></ul></div></div></div></div>
-           </div>
-       </div>
-       <!-- /.box-body -->
-     </div>
-     <!-- /.box -->
+            </div>
+            <div class="col-4">
+                <div class="box">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Add Brands</h3>
+                    </div>
+                    <form action="{{ route('brand.store') }}"
+                    method="post" enctype="multipart/form-data">
+                        @csrf
+                        <div class="col-12">
+                            <div class="form-group">
+                                <h5>Brand Name Eng
+                                    <span class="text-danger">*</span>
+                                </h5>
+                                <div class="controls">
+                                    <input type="text"
+                                    name="brand_name_en" class="form-control"
+                                    >
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <h5>Brand Name Ind
+                                    <span class="text-danger">*</span>
+                                </h5>
+                                <div class="controls">
+                                    <input type="text"
+                                    name="brand_name_ind" class="form-control"
+                                    >
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <h5>Brand Image
+                                    <span class="text-danger">*</span>
+                                </h5>
+                                <div class="controls">
+                                    <input type="file" name="brand_image" class="form-control">
+                                </div>
+                            </div>
+                            <div class="text-xs-right">
+                                <input type="submit" value="Add Data"
+                                class="btn btn-primary btn-rounded
+                                mb-5">
+                           </div>
+                        </div>
+                    </form>
 
-   </div>
-</div>
-</section>
-   @endsection
+                </div>
+                <!-- /.box -->
+
+            </div>
+        </div>
+    </section>
+@endsection
