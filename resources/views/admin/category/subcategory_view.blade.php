93c563cc192fb184427c40dc09e7625690f1efce
aldoprogrammer <aldobesma@gmail.com>
2024-02-12 10:46:37 +0700

    update

---
+++ resources/views/admin/category/subcategory_view.blade.php
@@ -0,0 +1,158 @@
+@extends('admin.admin_master')
+@section('content')
+    <section class="content">
+        <div class="row">
+            <div class="col-12 col-lg-8">
+                <div class="box">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Data Table SubCategory</h3>
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="table-responsive">
+                            <div id="example1_wrapper" class="dataTables_wrapper container-fluid dt-bootstrap4">
+                                <div class="row">
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <table id="example1" class="table table-bordered table-striped dataTable"
+                                            role="grid" aria-describedby="example1_info">
+                                            <thead>
+                                                <tr role="row">
+                                                    <th>Category</th>
+                                                    <th>SubCategory En</th>
+                                                    <th>SubCategory Ind</th>
+                                                    <th>Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                @foreach ($subcategory as $item)
+                                                    <tr role="row" class="odd">
+                                                        <td>{{ $item->category_id }}</td>
+                                                        <td>{{ $item->category_name_en }}</td>
+                                                        <td>{{ $item->category_name_ind }}</td>
+
+                                                        <td>
+                                                            <a href="{{ route('category.edit', $item->id) }}"
+                                                                class="btn btn-info">
+                                                            <i class="fa fa-edit"></i></a>
+                                                            <a href="{{ route('category.delete', $item->id) }}"
+                                                            id="delete"
+                                                                class="btn btn-danger">
+                                                            <i class="fa fa-trash"></i>
+                                                            </a>
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
+
+
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-12 col-md-5">
+                                        <div class="dataTables_info" id="example1_info" role="status" aria-live="polite">
+                                            Showing 1 to 10 of 57 entries</div>
+                                    </div>
+                                    <div class="col-sm-12 col-md-7">
+                                        <div class="dataTables_paginate paging_simple_numbers" id="example1_paginate">
+                                            <ul class="pagination">
+                                                <li class="paginate_button page-item previous disabled"
+                                                    id="example1_previous"><a href="#" aria-controls="example1"
+                                                        data-dt-idx="0" tabindex="0" class="page-link">Previous</a></li>
+                                                <li class="paginate_button page-item active"><a href="#"
+                                                        aria-controls="example1" data-dt-idx="1" tabindex="0"
+                                                        class="page-link">1</a></li>
+                                                <li class="paginate_button page-item "><a href="#"
+                                                        aria-controls="example1" data-dt-idx="2" tabindex="0"
+                                                        class="page-link">2</a></li>
+                                                <li class="paginate_button page-item "><a href="#"
+                                                        aria-controls="example1" data-dt-idx="3" tabindex="0"
+                                                        class="page-link">3</a></li>
+                                                <li class="paginate_button page-item "><a href="#"
+                                                        aria-controls="example1" data-dt-idx="4" tabindex="0"
+                                                        class="page-link">4</a></li>
+                                                <li class="paginate_button page-item "><a href="#"
+                                                        aria-controls="example1" data-dt-idx="5" tabindex="0"
+                                                        class="page-link">5</a></li>
+                                                <li class="paginate_button page-item "><a href="#"
+                                                        aria-controls="example1" data-dt-idx="6" tabindex="0"
+                                                        class="page-link">6</a></li>
+                                                <li class="paginate_button page-item next" id="example1_next"><a
+                                                        href="#" aria-controls="example1" data-dt-idx="7"
+                                                        tabindex="0" class="page-link">Next</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+
+            </div>
+            <div class="col-12 col-lg-4">
+                <div class="box">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Add SubCategory</h3>
+                    </div>
+                    <form action="{{ route('category.store') }}"
+                    method="post">
+                        @csrf
+                        <div class="col-12">
+                            <div class="form-group">
+                                <h5>Category List</h5>
+                                <div class="controls">
+                                    <select name="category_id"
+                                    id="" class="form-control">
+                                        <option value="">--- Pilih Category ---</option>
+                                        <option value="">1</option>
+                                        <option value="">2</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group mt-2">
+                                <h5>SubCategory Name Eng
+                                    <span class="text-danger">*</span>
+                                </h5>
+                                <div class="controls">
+                                    <input type="text"
+                                    name="subcategory_name_en" class="form-control"
+                                    >
+                                    @error('subcategory_name_en')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <h5>SubCategory Name Ind
+                                    <span class="text-danger">*</span>
+                                </h5>
+                                <div class="controls">
+                                    <input type="text"
+                                    name="subcategory_name_ind" class="form-control"
+                                    >
+                                    @error('subcategory_name_ind')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="text-xs-right">
+                                <input type="submit" value="Add Data"
+                                class="btn btn-primary btn-rounded
+                                mb-5">
+                           </div>
+                        </div>
+                    </form>
+
+                </div>
+                <!-- /.box -->
+
+            </div>
+        </div>
+    </section>
+@endsection
+
