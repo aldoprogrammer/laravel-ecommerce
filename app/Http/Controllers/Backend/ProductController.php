2e99a03c287e8cce9ad8e9887c729b91c99f520c
aldoprogrammer <aldobesma@gmail.com>
2024-02-23 08:38:41 +0700

    update

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\SubCategory;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function addProduct()
     {
-        // return view('backend.product.add-product');
+        $categories = Category::latest()->get();
+        $subcategories = SubCategory::latest()->get();
+        $brands = Brand::latest()->get();
+        return view('admin.product.product_add', compact('categories', 'subcategories', 'brands'));
     }
 }
